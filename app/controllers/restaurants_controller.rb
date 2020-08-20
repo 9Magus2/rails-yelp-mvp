9a90dcb9f0ad7aa1f268f005746702bf0ae292f5
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-08-20 20:31:18 +0200

    yelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,7 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @reviews = @restaurant.reviews
+    
   end
 
   def create
@@ -23,6 +24,6 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :rating) 
+    params.require(:restaurant).permit(:name, :address) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants, only:[:index, :new, :show, :create] do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:create, :new]
   end
-
+  resources :reviews, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
